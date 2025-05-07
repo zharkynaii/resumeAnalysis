21981f1fe640a9260650f88f23aa2eee75c73846
zharkynaii <zharkynai270205@gmail.com>
2025-05-07 23:14:06 +0500

    ✅ Safe version of requirements.txt for Render deployment

--- pyresparser/resume_parser.py
+++ pyresparser/resume_parser.py
@@ -15,8 +15,13 @@ class ResumeParser(object):
         skills_file=None,
         custom_regex=None
     ):
-        nlp = spacy.load('en_core_web_sm')
-        custom_nlp = spacy.load(os.path.dirname(os.path.abspath(__file__)))
+        try:
+            nlp = spacy.load("en_core_web_sm")
+        except OSError:
+            download("en_core_web_sm")
+            nlp = spacy.load("en_core_web_sm")
+        # nlp = spacy.load('en_core_web_sm')
+        # custom_nlp = spacy.load(os.path.dirname(os.path.abspath(__file__)))
         self.__skills_file = skills_file
         self.__custom_regex = custom_regex
         self.__matcher = Matcher(nlp.vocab)
