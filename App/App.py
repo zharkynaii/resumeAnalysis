70d2985165bb980c1a2ed15e48aae6cf006ba8dc
zharkynaii <zharkynai270205@gmail.com>
2025-05-07 18:35:03 +0500

    ✅ Safe version of requirements.txt for Render deployment

--- App/App.py
+++ App/App.py
@@ -26,6 +26,7 @@ from PIL import Image
 from Courses import ds_course,web_course,android_course,ios_course,uiux_course,resume_videos,interview_videos
 import nltk
 nltk.download('stopwords')
+nltk.download('punkt')
 
 
 ###### Preprocessing functions ######
